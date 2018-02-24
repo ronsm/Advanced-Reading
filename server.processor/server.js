2292854b072869ec5256aaed3ff42060dcc44782
Ronnie Smith <ronniesmith@outlook.com>
2018-02-24 14:36:26 +0000

    Added Kalman Filter

--- server.processor/server.js
+++ server.processor/server.js
@@ -17,6 +17,10 @@
 var app = require('express')();
 var http = require('http').Server(app);
 
+var KalmanFilter = require('kalmanjs').default;
+var kf = new KalmanFilter();
+kf.filter(2);
+
 // MongoDB
 var dbName = "AR";
 var mongo = require('mongodb');
@@ -35,8 +39,8 @@ function runProcessor(){}
 setInterval(processor, 1000);
 
 function processor(){
-    var latestReading = getLatestBeaconReading();
-    console.log(latestReading);
+    var latestReading = getEstimatedDistanceFromBeacon(1, true);
+    //console.log(latestReading);
 }
 
 /* 
@@ -48,7 +52,7 @@ function getAllBeaconReadings(){
 
     mongoClient.connect(con_url, function(err, db){
         if(err) throw err;
-        db.collection("Beacon_RSSI_Readings").find().toArray(function(err, docs) {
+        db.collection("Beacon_RSSI_Readings_2").find().toArray(function(err, docs) {
             console.log(JSON.stringify(docs));
             db.close();
         });
@@ -56,20 +60,67 @@ function getAllBeaconReadings(){
 
 }
 
-function getLatestBeaconReading(){
+function getEstimatedDistanceFromBeacon(beaconId, filter){
+
+    getLatestBeaconReading(filter, function(results) {
+        var averageRssi = 0;
+
+        var sum = 0;
+        for(var i = 0; i < 10; i++){
+            sum = sum + Math.abs(results[i]);
+        }
+
+        averageRssi = sum / 10;
+    
+        console.log(averageRssi);
+
+    });
+
+}
+
+function getLatestBeaconReading(filter, callback){
 
     mongoClient.connect(con_url, function(err, db){
         if(err) throw err;
-        var latestReading = db.collection("Beacon_RSSI_Readings").find().limit(1).sort({$natural:-1});
+        //var latestReading = db.collection("Beacon_RSSI_Readings_2").find().limit(10).sort({$natural:-1});
 
-        latestReading.toArray(function(err, results) {
+        var latestReadings = db.collection("Beacon_RSSI_Readings_2").find({}, { _id: 0, rssi: 1 }).sort({ $natural: -1 }).limit(10);
+
+        latestReadings = latestReadings.toArray(function(err, results) {
             if (err) throw err;
             console.log('%j', results);
             db.close();
-            return results;
+
+            var simpleResults = [];
+            for(var i = 0; i < 10; i++){
+                simpleResults[i] = results[i].rssi;
+            }
+
+            results = simpleResults;
+
+            console.log(results);
+
+            if(filter){
+                results = kalmanFilterReadings(results);
+            }
+
+            console.log(results);
+
+            callback(results);
+
         });
     });
 
+}
+
+function kalmanFilterReadings(noisyData){
+    var kalmanFilter = new KalmanFilter({R: 0.01, Q: 3});
+    
+    var kalmanData = noisyData.map(function(v) {
+        return kalmanFilter.filter(v);
+    });
+
+    return kalmanData;
 }
 
 /* 
@@ -78,6 +129,7 @@ function getLatestBeaconReading(){
  */
 
 function calculateDistance(rssi) {
+
     var txPower = -59;
 
     if (rssi == 0){
@@ -93,4 +145,5 @@ function calculateDistance(rssi) {
         var distance =  ((0.89976) * Math.pow(ratio, 7.7095)) + 0.111;
         return distance;
     }
+
 } 
